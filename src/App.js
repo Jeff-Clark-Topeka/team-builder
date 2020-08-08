3c758ff86957d146683b8031d21c03599bdbd3c9
Jeff-Clark-Topeka <jeffclarktopeka@yahoo.com>
2020-08-08 18:30:27 -0500

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './components/Form';
+import Forms from './components/Forms';
 
 function App() {
+
+  const [forms, setForms] = useState([]) 
+
+  const addForm = form => {
+    
+    const newForm = {
+      id: Date.now(),
+      name: form.name,
+      email: form.email,
+      role: form.role
+    };
+    
+    setForms([...forms, newForm])
+  
+  }
+
   return (
     <div>
       <h2>This is my Form App.</h2>
+      <Form addForm={addForm}/>
+      <Forms forms={forms}/>
     </div>
   );
 }
